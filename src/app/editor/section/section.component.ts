6934c46ea11aa2e4deaec6f02041633071f74097
Mincho Milev <mincho.milev@gmail.com>
2021-10-22 12:23:25 +0300

    [PENSOFT-12] update default temaplate

--- src/app/editor/section/section.component.ts
+++ src/app/editor/section/section.component.ts
@@ -1,114 +1,16 @@
 import { AfterContentInit, AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { EditSectionService } from '../dialogs/edit-section-dialog/edit-section.service';
-import { TaxonomicCoverageComponent } from '../formioComponents/taxonomic-coverage/taxonomic-coverage.component';
 import { ProsemirrorEditorsService } from '../services/prosemirror-editors.service';
 import { YdocService } from '../services/ydoc.service';
 import { articleSection, editorData, sectionContentData, taxa, taxonomicCoverageContentData, titleContentData } from '../utils/interfaces/articleSection';
 
-let staticProsemirrorNodeHTMLyarn  = `<div contenteditable="true" translate="no" class="ProseMirror ProseMirror-example-setup-style">
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="" data-validations="max-size:100" data-regex="^[a-zA-Z ]{0,15}$">Idddddddddddddd</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="" data-validations="" data-regex="">qweqqe</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="" data-validations="" data-regex="">wewqee</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="" data-validations="" data-regex="">lab</p>
-</div>`
-let smallHTML = `<div contenteditable="true" translate="no" class="ProseMirror ProseMirror-example-setup-style">
-<p class="set-align-left" data-validations="max-size:100" data-regex="^[a-zA-Z ]{0,15}$" data-id="" data-track="[]" data-group="" data-viewid="">I </p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweqqwe; idqweeqweqwe </p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">wewqeqwe</p>
-<input_container1 data-input-id="">
-    <input_label style="color:gray" contenteditable="false">label</input_label>
-    <input_placeholder style="border:1px solid black;margin-left:3px;margin-right:3px">placeholder</input_placeholder>
-</input_container1>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qw</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">eeewEEweqweEQ<span class="comment" data-id="4191f684-092c-486a-ad11-63f97b3e690f" data-conversation="[]" data-viewid="" data-group="">WEEEw</span>; idqweeqweqwe</p>
-<div class="tableWrapper">
-    <table style="min-width: 75px;">
-        <colgroup>
-            <col>
-            <col>
-            <col>
-        </colgroup>
-        <tbody>
-            <tr>
-                <td>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">ee<sup>ee</sup><sub><sup>wewe</sup>eewe</sub>wewe</p>
-                </td>
-                <td>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">ew</p>
-                </td>
-                <td>
-                    <p class="set-align-right" data-id="" data-track="[]" data-group="" data-viewid="">qwe</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid=""><br></p>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweeqweqwe</p>
-                </td>
-                <td>
-                    <p class="set-align-center" data-id="" data-track="[]" data-group="" data-viewid="">
-                        <math-inline class="math-node" title="" contenteditable="false"><span class="math-render"><span class="katex"><span class="katex-mathml"><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>d</mi><mi>q</mi><mi>w</mi><mi>d</mi></mrow><annotation encoding="application/x-tex">dqwd</annotation></semantics></math></span>
-                            <span
-                                class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height: 0.88888em; vertical-align: -0.19444em;"></span><span class="mord mathnormal">d</span><span class="mord mathnormal" style="margin-right: 0.02691em;">qw</span><span class="mord mathnormal">d</span></span>
-                                </span>
-                                </span>
-                                </span><span class="math-src"></span></math-inline>dqwdqwd</p>
-                </td>
-                <td>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qwe</p>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">placeholder</p>
-                </td>
-                <td>
-                    <ol>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">werwerwer</p>
-                        </li>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">wer</p>
-                        </li>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">werwerwer</p>
-                        </li>
-                    </ol>
-                </td>
-                <td>
-                    <ul>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweqweqwe</p>
-                        </li>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qwe</p>
-                        </li>
-                        <li>
-                            <p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweqwe</p>
-                        </li>
-                    </ul>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid=""><span class="comment" data-id="4191f684-092c-486a-ad11-63f97b3e690f" data-conversation="[]" data-viewid="" data-group="">wwe</span>qEEqwe</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweq</p>
-<input_container1 data-input-id="">
-    <input_label style="color:gray" contenteditable="false">label</input_label>
-    <input_placeholder style="border:1px solid black;margin-left:3px;margin-right:3px">placeholder</input_placeholder>
-</input_container1>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">; idq</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">wewqeqwe</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">labelplaceholdereewEEweqweEQWEEEwwweqEE</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qweq</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">wewqeqwe</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">labelplaceholder</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qw</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">e</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">qw</p>
-<p class="set-align-left" data-id="" data-track="[]" data-group="" data-viewid="">e</p>
-</div>`
+let smallHTML = `<ul>
+<li><span>${data.textField1}</span>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+<li><span>${data.textField2}</span>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+<li><span>${data.textField3}</span>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+<li><span>${data.textField4}</span>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+</ul>`
+
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
