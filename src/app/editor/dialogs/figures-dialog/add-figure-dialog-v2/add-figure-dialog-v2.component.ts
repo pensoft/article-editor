a32845041a405d712b7d9262ae5e738b99a71b91
Mihail Antonov <mihail.d.antonov@gmail.com>
2023-02-03 13:34:25 +0200

    PENSOFT-420 - figure inside editor styling - wip

--- src/app/editor/dialogs/figures-dialog/add-figure-dialog-v2/add-figure-dialog-v2.component.ts
+++ src/app/editor/dialogs/figures-dialog/add-figure-dialog-v2/add-figure-dialog-v2.component.ts
@@ -21,8 +21,8 @@ let figuresHtmlTemplate = `
     <figure-components-container contenteditablenode="false">
       <ng-container *ngFor="let figure of data.figureComponents;let i = index">
         <ng-container *ngIf="figure">
-            <figure-component [attr.actual_number]="figure.container.componentNumber" [attr.component_number]="i" contenteditablenode="false" [attr.viewed_by_citat]="data.viewed_by_citat||''">
             <code>{{getCharValue(i)}}</code>
+            <figure-component [attr.actual_number]="figure.container.componentNumber" [attr.component_number]="i" contenteditablenode="false" [attr.viewed_by_citat]="data.viewed_by_citat||''">
             <img *ngIf="figure.container.componentType == 'image'" src="{{figure.container.url}}" alt="" title="default image" contenteditable="false" draggable="true">
                <iframe *ngIf="figure.container.componentType == 'video'" src="{{figure.container.url}}" controls="" contenteditable="false" draggable="true"></iframe>
             </figure-component>
