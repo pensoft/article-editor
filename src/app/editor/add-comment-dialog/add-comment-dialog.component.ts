5a0baaa597136e1284586ce259ee9bcad7b7a0c0
mincho.milev <mincho.milev@scalewest.com>
2023-02-16 19:00:20 +0200

    [PENSOFT-501] combine katex formula input and added preview

--- src/app/editor/add-comment-dialog/add-comment-dialog.component.ts
+++ src/app/editor/add-comment-dialog/add-comment-dialog.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as katex from 'katex';
 
 interface DialogData {
@@ -15,27 +16,54 @@ interface DialogData {
 export class AddCommentDialogComponent {
 
   text = new FormControl('', [Validators.required]);
+  mathType = new FormControl('', [Validators.required]);
 
   type: string
 
-  mathType: string
+  mathPreviews = [];
 
-  @ViewChild('mathPreview') mathPreview: ElementRef;
+  selectedMathExample = 'math_inline';
 
   constructor(
     public dialogRef: MatDialogRef<AddCommentDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private sanitizer: DomSanitizer) {
     this.type = data.type
-    this.mathType = data.mathType
 
     this.text.valueChanges.subscribe((katexFormula) => {
+      this.mathPreviews = [];
       if (katexFormula) {
-        katex.render(katexFormula, this.mathPreview.nativeElement, {
-          displayMode: true,
-          throwOnError: false,
-        })
+        let singleLineExample;
+        try {
+          const singlelineFormula = katex.renderToString(katexFormula)
+          singleLineExample = {
+            value: 'math_inline',
+            html: "Lorem Ipsum lorem ipsum " + singlelineFormula + " lorem ipsum lorem ipsum.",
+          }
+          this.mathPreviews.push(singleLineExample);
+          this.selectedMathExample = 'math_inline';
+        } catch { }
+
+        try {
+          const multilineFormula = katex.renderToString(katexFormula, {
+            displayMode: true,
+          })
+          const multilineExample = {
+            value: 'math_display',
+            html: "Lorem Ipsum lorem ipsum:" + multilineFormula + "ipsum lorem ipsum.",
+          }
+          this.mathPreviews.push(multilineExample);
+
+          if (!singleLineExample) {
+            this.selectedMathExample = 'math_display';
+          }
+        } catch { }
       }
     })
+  }
+
+  sanitize(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
   onNoClick(): void {
