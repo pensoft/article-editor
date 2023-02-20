675527a65690cd2eb51250741a2310b73352831d
Mincho Milev <mincho.milev@scalewest.com>
2023-02-20 16:57:03 +0200

    Add dynamic labels in section edit popup title and on formio textfields with key "sectionTreeTitle".

--- src/app/formio-angular-material/components/label/label.component.ts
+++ src/app/formio-angular-material/components/label/label.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -6,6 +6,29 @@ import { Component, Input } from '@angular/core';
   templateUrl: './label.component.html',
   styleUrls: ['./label.component.css']
 })
-export class LabelComponent {
+export class LabelComponent implements OnInit {
   @Input() instance:any;
+  label
+  constructor(){
+
+  }
+  contaniner:HTMLDivElement
+  getTextContent(html){
+    if(!this.contaniner){
+      this.contaniner = document.createElement('div')
+    }
+    this.contaniner.innerHTML = html;
+    this.label = this.contaniner.textContent
+  }
+
+  ngOnInit(): void {
+    if(this.instance && this.instance.path == "sectionTreeTitle"){
+      this.getTextContent(this.instance.getValue())
+      this.instance.events.addListener('formio.change',(ch,ch2)=>{
+        if(ch2&&ch2.changed&&ch2.changed.instance == this.instance){
+          this.getTextContent(this.instance.getValue())
+        }
+      })
+    }
+  }
 }
