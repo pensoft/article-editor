176fdd96d36c2e252d0c66dfeecd73be8256684b
Mincho Milev <mincho.milev@scalewest.com>
2023-03-02 13:02:58 +0200

    Change menu item logic and persist selection when dialog is open

--- src/app/editor/add-link-dialog/add-link-dialog.component.ts
+++ src/app/editor/add-link-dialog/add-link-dialog.component.ts
@@ -1,12 +1,13 @@
-import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DetectFocusService } from '../utils/detectFocusPlugin/detect-focus.service';
 @Component({
   selector: 'app-add-link-dialog',
   templateUrl: './add-link-dialog.component.html',
   styleUrls: ['./add-link-dialog.component.scss']
 })
-export class AddLinkDialogComponent implements AfterViewInit{
+export class AddLinkDialogComponent implements AfterViewInit, OnDestroy{
 
   text = new FormControl('', [Validators.required]);
   @ViewChild('linkurlinput', { read: ElementRef }) linkurlinput?: ElementRef;
@@ -14,16 +15,13 @@ export class AddLinkDialogComponent implements AfterViewInit{
   type :string
   constructor(public dialogRef: MatDialogRef<AddLinkDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private ref:ChangeDetectorRef) {
+    private ref:ChangeDetectorRef,
+    private detectFocusService: DetectFocusService) {
       this.type = data.type
      }
 
-
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
   ngAfterViewInit(): void {
+    this.detectFocusService.setSelectionDecorationOnLastSelecctedEditor();
     this.linkurlinput.nativeElement.focus()
     this.ref.detectChanges();
   }
@@ -34,5 +32,9 @@ export class AddLinkDialogComponent implements AfterViewInit{
     }
 
     return '';
+  }
+
+  ngOnDestroy(): void {
+    this.detectFocusService.removeSelectionDecorationOnLastSelecctedEditor();
   }
 }
