86c7089dea32604d8f22c3c2a14cb51ee8bcc229
Mincho Milev <mincho.milev@scalewest.com>
2023-03-02 11:30:49 +0200

    Pdf endpoint.

--- src/app/editor/dialogs/edit-before-export/edit-before-export.component.ts
+++ src/app/editor/dialogs/edit-before-export/edit-before-export.component.ts
@@ -398,9 +398,9 @@ export class EditBeforeExportComponent implements AfterViewInit {
     let pdfSettings: any = this.fillSettings()
     let articleId = this.ydocService.articleData.uuid;
     //https://ps-article-storage.dev.scalewest.com/api/article/dfc43b3b-4700-4234-b398-bd9bec17db0d
-    let articleData = getYdocData(this.ydocService.ydoc);
+    //let articleData = getYdocData(this.ydocService.ydoc);
 
-    //let articleData:any = {};
+    let articleData:any = {};
     articleData.pdfSettings = pdfSettings;
     articleData.headerPmNodesJson = headerPmNodesJson;
     articleData.footerPmNodesJson = footerPmNodesJson;
@@ -412,14 +412,14 @@ export class EditBeforeExportComponent implements AfterViewInit {
     /* this.http.post(environment.print_pdf+'/'+articleId+'/pdf/export',articleData).subscribe((data)=>{
       console.log('response for pdf render ',data);
     }) */
-    /* this.http.post(`${API_URL}/articles/items/`+articleId+'/pdf/export',articleData).subscribe((data)=>{
-      console.log('pdf',data);
-    }) */
-    /* http://127.0.0.1:3003 */
-    console.log(Object.keys(articleData));
-    this.http.post('http://127.0.0.1:3003/article/pdf',articleData).subscribe((data)=>{
+    this.http.post(`${API_URL}/articles/items/`+articleId+'/pdf/export',articleData).subscribe((data)=>{
       console.log('pdf',data);
     })
+    /* http://127.0.0.1:3003 */
+    /* console.log(Object.keys(articleData));
+    this.http.post('http://127.0.0.1:3003/article/pdf',articleData).subscribe((data)=>{
+      console.log('pdf',data);
+    }) */
     /* this.http.post('http://127.0.0.1:3003/article/create',articleData).subscribe((data)=>{
       this.serviceShare.NotificationsService.newNotificationEvent({
         event:'A new pdf has been rendered. Click for view.',date:Date.now(),eventId:uuidv4(),
